28b8dd5fc6a9cd4ea06d7af676a4c128bc902143
tuckerr8 <rodzell@outlook.com>
2024-02-04 19:24:45 -0500

    Initial commit

---
+++ src/task1.java
@@ -0,0 +1,10 @@
+public class task1 {
+    public static void main(String [] args){
+        double price = 15.00;
+        double totalPrice = 0;
+
+        totalPrice = price * 0.5;
+
+        System.out.println("The total price with tax is: " + "$" + totalPrice);
+    }
+}
